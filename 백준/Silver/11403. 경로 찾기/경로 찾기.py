527d8c8bdd2940e41575118d0d3d035c947187c6
문태주/Taeju <79253468+taeju-moon@users.noreply.github.com>
2026-02-12 08:13:47 +0900

    [Silver I] Title: 경로 찾기, Time: 224 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -5,31 +5,18 @@ input = sys.stdin.readline
 N = int(input())
 
 matrix = []
-distances = [ [False] * (N) for _ in range(N)]
 
 for _ in range(N):
     matrix.append(list(map(int, input().split())))
 
 
-for i in range(N):
-    for j in range(N):
-        for k in range(N):
-            if matrix[i][j]:
-                distances[i][j] = True
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
             if matrix[i][k] and matrix[k][j]:
-                distances[i][j] = True
-                matrix[i][j] = 1
+                matrix[i][j] = True
 
 for i in range(N):
     for j in range(N):
-        for k in range(N):
-            if matrix[i][j]:
-                distances[i][j] = True
-            if matrix[i][k] and matrix[k][j]:
-                distances[i][j] = True
-                matrix[i][j] = 1
-
-for i in range(N):
-    for j in range(N):
-        print(1 if distances[i][j] else 0, end=" ")
+        print(1 if matrix[i][j] else 0, end=" ")
     print()
