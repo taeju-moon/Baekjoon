43c02ffe47288908440c5301b40906dc08b11800
문태주/Taeju <79253468+taeju-moon@users.noreply.github.com>
2026-02-12 08:07:00 +0900

    [Silver I] Title: 경로 찾기, Time: 960 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,45 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+
+matrix = []
+distances = [ [False] * (N) for _ in range(N)]
+
+for _ in range(N):
+    matrix.append(list(map(int, input().split())))
+
+
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            if matrix[i][j]:
+                distances[i][j] = True
+            if matrix[j][i]:
+                distances[j][i] = True
+            if matrix[i][k] and matrix[k][j]:
+                distances[i][j] = True
+                matrix[i][j] = 1
+            if matrix[j][k] and matrix[k][i]:
+                distances[j][i] = True
+                matrix[j][i] = 1
+
+for i in range(N):
+    for j in range(N):
+        for k in range(N):
+            if matrix[i][j]:
+                distances[i][j] = True
+            if matrix[j][i]:
+                distances[j][i] = True
+            if matrix[i][k] and matrix[k][j]:
+                distances[i][j] = True
+                matrix[i][j] = 1
+            if matrix[j][k] and matrix[k][i]:
+                distances[j][i] = True
+                matrix[j][i] = 1
+
+for i in range(N):
+    for j in range(N):
+        print(1 if distances[i][j] else 0, end=" ")
+    print()
